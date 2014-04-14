4ca3c6802d956509d4d54cd6787dd43a2fd2cd51
Krzysztof Sobkowiak <krzys.sobkowiak@gmail.com>
2014-04-14 22:50:24 +0200

    SM-2281 Upgrade to Karaf 2.3.5
    (cherry picked from the commit 5b901cadd5b2531250b316cccae8cf685983b040)

--- itests/src/test/scala/org/apache/servicemix/itests/IntegrationTestConfigurations.scala
+++ itests/src/test/scala/org/apache/servicemix/itests/IntegrationTestConfigurations.scala
@@ -51,7 +51,7 @@ trait IntegrationTestConfigurations {
       karafDistributionConfiguration().
         frameworkUrl(
           maven().groupId("org.apache.servicemix").artifactId(artifact).`type`("zip").versionAsInProject()).
-        karafVersion("2.3.4").name("Apache ServiceMix (${name})").
+        karafVersion("2.3.5").name("Apache ServiceMix (${name})").
         unpackDirectory(new File(s"target/pax-exam/${artifact}")).
         useDeployFolder(false),
       keepRuntimeFolder(),
