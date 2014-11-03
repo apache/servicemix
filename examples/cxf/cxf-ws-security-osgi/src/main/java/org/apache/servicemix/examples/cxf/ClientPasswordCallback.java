ba1ee8ea5296621e28fed4bc6ff803b5af8c8b26
Krzysztof Sobkowiak <krzys.sobkowiak@gmail.com>
2014-11-03 22:11:04 +0100

    SM-2368: Upgrade to Camel 2.14.0 and CXF 3.0.2
    (Cherry-picked from the commit 76ed680ce48f141e4a6f2778a4882b51ef0edd33)

--- examples/cxf/cxf-ws-security-osgi/src/main/java/org/apache/servicemix/examples/cxf/ClientPasswordCallback.java
+++ examples/cxf/cxf-ws-security-osgi/src/main/java/org/apache/servicemix/examples/cxf/ClientPasswordCallback.java
@@ -19,11 +19,13 @@
 
 package org.apache.servicemix.examples.cxf;
 
+import org.apache.wss4j.common.ext.WSPasswordCallback;
+
 import java.io.IOException;
 import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.UnsupportedCallbackException;
-import org.apache.ws.security.WSPasswordCallback;
+
 
 public class ClientPasswordCallback implements CallbackHandler {
 
